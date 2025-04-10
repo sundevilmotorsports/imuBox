21dad3d77edebe32a901488da170e05205ecaccd
IWumbo203 <ryansleigh2003@gmail.com>
2025-04-10 03:01:30 -0700

    readjusted files and added CAN support

---
+++ imubox/Debug/Core/Src/ism330dhcx.d
@@ -0,0 +1,4 @@
+Core/Src/ism330dhcx.o: ../Core/Src/ism330dhcx.c ../Core/Inc/ism330dhcx.h \
+ ../Core/Inc/ism330dhcx_reg.h
+../Core/Inc/ism330dhcx.h:
+../Core/Inc/ism330dhcx_reg.h:
---
+++ imubox/Debug/Core/Src/ism330dhcx_reg.d
@@ -0,0 +1,3 @@
+Core/Src/ism330dhcx_reg.o: ../Core/Src/ism330dhcx_reg.c \
+ ../Core/Inc/ism330dhcx_reg.h
+../Core/Inc/ism330dhcx_reg.h:
